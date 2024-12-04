370cdc4a4741c35eca183061b89926442d620df3
Vincent CHEN <chenv.pro@gmail.com>
2024-12-04 15:01:34 +0100

    UserModel terminé mais pas testé.

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -1,22 +1,45 @@
 <?php
     class UserModel {
         public function logUser(string $email, string $motdepasse): array {
-            
+            $email = htmlspecialchars(trim($POST['email']));
+            $pwd = htmlspecialchars(trim($POST['pwd']));
+
+            $sql = 'SELECT email, motdepasse FROM users WHERE email = :email LIMIT 1';
+            $params = [
+                'email' => $email
+            ];
+
+            $select = $this->co->prepare($sql);
+            $select->execute($params);
+
+            if($select->rowCount() == 1){
+                $user = $select->fetch();
+                if(password_verify($pwd, $user['motdepasse'])){
+                    $_SESSION['email'] = $user['email'];
+                    return true;
+                }
+                else{
+                    $_SESSION['errors'][] = 'Identifiant ou mot de passe incorrect';
+                    return false;
+                }
+            } else {
+                return false;
+            }
         }
 
         public function createUser(array $data) : bool {
             $prenom = htmlspecialchars(trim($POST['prenom']));
             $nom = htmlspecialchars(trim($POST['nom']));
             $email = htmlspecialchars(trim($POST['email']));
-            $pwd = htmlspecialchars(trim($POST['pwd']));
+            $motdepasse = htmlspecialchars(trim($POST['motdepasse']));
             $role = htmlspecialchars(trim($POST['role']));
 
-            $sql = 'INSERT INTO users(prenom, nom, email, motdepasse, role) VALUES (:prenom, :nom, :email, :pwd)';
+            $sql = 'INSERT INTO users(prenom, nom, email, motdepasse, role) VALUES (:prenom, :nom, :email, :motdepasse)';
             $params = [
                 'prenom' => $prenom,
                 'nom' => $nom,
                 'email' => $email,
-                'pwd' => password_hash(
+                'motdepasse' => password_hash(
                     $pwd, 
                     PASSWORD_BCRYPT
                     )
