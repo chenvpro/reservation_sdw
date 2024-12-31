4c07cba73f63d006953c364a7dc29958f974d1ae
Vincent CHEN <chenv.pro@gmail.com>
2024-12-31 15:11:24 +0100

    Avancement sur différentes pages

---
+++ app/views/user/login.php
@@ -0,0 +1,10 @@
+<h1>Login</h1>
+<form action="user/login" method="POST">
+    <label for="email">Email :</label>
+    <input type="email" name="email" id="email" required>
+    <br>
+    <label for="motdepasse">Mot de passe :</label>
+    <input type="password" name="motdepasse" id="motdepasse" required>
+    <br>
+    <input type="submit" name="log_user" value="Se connecter">
+</form>
--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-
-?>
+    session_start();
+    
+    // Chargement de l'autoload de vendor
+    require './vendor/autoload.php';
+    // Chargement des variables d'environnement
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__); //! réajouter le package vlucas via ligne de commande
+    $dotenv->load();
+     
+    // Chargement de notre autoload
+    require_once './app/utils/Autoload.php';
+    // Appel de la méthode register qui va recenser notre autoload
+    Autoload::register();
+    
+    $router = new Router();
+    $router->dispatch($_SERVER['REQUEST_URI']);
