b829d7999c5fe7e21b1ac8d88e6275002c418bcf
Vincent CHEN <chenv.pro@gmail.com>
2025-01-02 12:03:48 +0100

    Progression de la rédaction des fonctions

--- app/controllers/ReservationController.php
+++ app/controllers/ReservationController.php
@@ -1,18 +1,46 @@
 <?php
     class Reservation {
+
+        Use Render;
+
         public function index() {
             $reservationModel = new ReservationModel();
-            $reservations = $reservationModel->getReservationsByUserId($_SESSION['user']['id']); // ! à vérifier et comprendre
+            $reservations = $reservationModel->getReservationsByUserId($_SESSION['user']['id']);
             
-            // ! afficher
+            $data = [
+                'title' => 'Liste des réservations',
+                'reservations' => $reservations,
+                'isAdmin' => $_SESSION['user']
+            ]; 
+
+            $this->renderView('reservation/index', $data);
         }
 
         public function create(int $activityId) {
+            $reservationModel = new ReservationModel();
+            $createReservation = $reservationModel->createReservation($_SESSION['user']['id'], $activityId);
+
+            $data = [
+                'title' => 'Liste des réservations',
+                'reservations' => $createReservation,
+                'isAdmin' => $_SESSION['user']
+            ];
             
+            $this->renderView('reservation/list', $data);
         }
 
         public function show(int $id) {
             
+            $reservationModel = new ReservationModel();
+            $reservation = $reservationModel->getReservationsByUserId($id);
+            
+            $data = [
+                'title' => 'Détail de la réservation',
+                'reservation' => $reservation,
+                'isAdmin' => $_SESSION['user']
+            ];
+
+            $this->renderView('reservation/show', $data);
         }
 
         public function cancel (int $id) {
