c2aa80512d6e0c824cb1522fe3f3339301e7e549
Vincent CHEN <chenv.pro@gmail.com>
2025-01-02 12:03:27 +0100

    fonctions terminées

--- app/controllers/ActvityController.php
+++ app/controllers/ActvityController.php
@@ -8,10 +8,11 @@
             
             $data = [
                 'title' => 'Liste des activités',
-                'activities' => $activities
+                'activities' => $activities,
+                'isAdmin' => $_SESSION['user'] && $_SESSION['user']['role'] === 'admin'
             ];   
 
-            // ! afficher
+            $this->renderView('activity/show', $data);
         }
 
         public function show(int $id) {
@@ -20,17 +21,53 @@
 
             $data = [
                 'title' => 'Détails de l\'activité',
-                'activity' => $activityDetails
+                'activity' => $activityDetails,
+                'isAdmin' => $_SESSION['user'] && $_SESSION['user']['role'] === 'admin'
             ];
 
-            // ! afficher
-
+            $this->renderView('activity/show', $data);
         }
 
         public function update(int $id, array $data) {
+            if ($_SESSION['user']['role'] === 'admin') {
+                $db = new mysqli('localhost', 'root', "", "reservation_sdw");
+                
+                $name = $db->real_escape_string($data['name']);
+                $type_id = $db->real_escape_string($data['type_id']);
+                $places_disponibles = $db->real_escape_string($data['places_disponibles']);
+                $description = $db->real_escape_string($data['description']);
+                $datetime_debut = $db->real_escape_string($data['datetime_debut']);
+                $duree = $db->real_escape_string($data['duree']);
 
+                $sql = "UPDATE activities SET name = '$name', type_id = '$type_id', places_disponibles = '$places_disponibles', description = '$description', datetime_debut = '$datetime_debut', duree = '$duree' WHERE id = $id";
+                
+                if ($db->query($sql) === TRUE) {
+                    echo "Succès de la mise à jour";
+                } else {
+                    echo "Erreur dans la mise à jour";
+                }
+
+                $db->close();
+            } else {
+                echo "Vous n'avez pas les droits pour effectuer cette action";
+            }
         }
 
         public function cancel (int $id) {
+            if ($_SESSION['user']['role'] === 'admin') {
+                $db = new mysqli('localhost', 'root', "", "reservation_sdw");
+                
+                $sql = "DELETE FROM activities WHERE id = $id";
+                
+                if ($db->query($sql) === TRUE) {
+                    echo "Succès de la mise à jour";
+                } else {
+                    echo "Erreur dans la mise à jour";
+                }
+
+                $db->close();
+            } else {
+                echo "Vous n'avez pas les droits pour effectuer cette action";
+            }
         }
     }
